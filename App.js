2ef2b7159a6ed8427c79006a89429a4bfb9e961f
Rednux <rednux95@gmail.com>
2019-01-18 22:37:21 -0500

    LogoAndUsername
    Ready

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Dimensions, TextInput } from 'react-native';
+
+import BgImage from './assets/Background.png'
+import Logo from './assets/Logo2.png'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const {width: WIDTH} = Dimensions.get("window")
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your holman crack app!</Text>
-      </View>
+      <ImageBackground source={BgImage} style={styles.backgroundContainer}>
+      <View style={styles.LogoContain}>
+        <Image source={Logo} style={styles.logo}/>
+        <Text style={styles.logoText}>Cedro</Text>
+        <Text style={styles.logoTextSub}>tecnologies</Text>
+      </View>   
+
+      <View>
+        <Icon name={'md-person'} size={28} color={'rgb(255,255,255)'}  style={styles.inputIcon}/> 
+        <TextInput
+        style={styles.input}
+        placeholder={'username'}
+        placeholderTextColor={'rgb(255,255,255)'} 
+        underlineColorAndroid='transparent'></TextInput>
+      
+      </View>    
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundContainer:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width:null,
+    height:null,
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  LogoContain:{
+    alignItems: 'center'    
+  },
+  logoText:{
+    color: 'rgb(160,197,86)',
+    fontSize: 38,
+    fontWeight: '500',    
+    opacity: 0.8
+  },
+  logoTextSub:{
+    color: 'rgb(81,74,122)',
+    fontSize: 12,
+    fontWeight: '500',
+    marginBottom: 12,
+    opacity: 0.8
+  },
+  input:{
+    width: WIDTH -55,    
+    height: 45,
+    borderRadius:25,
+    paddingLeft:50,
+    backgroundColor: 'rgba(0,0,0,0.35)',
+    color: 'rgba(255,255,255,0.7)',
+    marginHorizontal:25
+  },
+  inputIcon:{
+    position: 'absolute',
+    top: 8,
+    left:40
+  },
+  logo:{
+    width:150,
+    height:150,
+    opacity: 0.9
   },
 });
